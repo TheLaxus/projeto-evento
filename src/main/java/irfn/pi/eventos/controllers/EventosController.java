71536d93b0a2bff59053ed8dea7cae01041dbff1
Lucas Barbosa <lucashp03@gmail.com>
2021-08-22 02:02:05 -0300

    Adicionando listagem de eventos

--- src/main/java/irfn/pi/eventos/controllers/EventosController.java
+++ src/main/java/irfn/pi/eventos/controllers/EventosController.java
@@ -1,22 +1,26 @@
 package irfn.pi.eventos.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import irfn.pi.eventos.models.Evento;
 import irfn.pi.eventos.repositories.EventoRepository;
 
 @Controller
+@RequestMapping("/eventos")
 public class EventosController {
 
 	@Autowired // quem cuida de criar e vincular o objeto
 	private EventoRepository er;
 
-	@GetMapping("/eventos/form") // metodo get
+	@GetMapping("/form") // metodo get
 	public ModelAndView viewCadastrarEvento() {
 		ModelAndView mv = new ModelAndView("eventos/formEvento");
 		mv.addObject("evento", new Evento());
@@ -24,7 +28,7 @@ public class EventosController {
 	}
 
 
-	@PostMapping("/eventos") // metodo post
+	@PostMapping // metodo post
 	public String adicionar(@ModelAttribute Evento evento) {
 
 		// receber dados do formulario
@@ -32,5 +36,15 @@ public class EventosController {
 		er.save(evento);
 
 		return "eventos/evento-adicionar";
+	}
+	
+	@GetMapping
+	public ModelAndView listar() {
+		
+		List<Evento> eventos = er.findAll();
+		ModelAndView mv = new ModelAndView("eventos/lista");
+		mv.addObject("eventos", eventos);
+		return mv;
+		
 	}
 }
