4eb2ccc83db570c12a8703fe9969af05c65dc847
Lucas Barbosa <lucashp03@gmail.com>
2021-08-22 00:03:50 -0300

    Melhorando aparencia e usando template do Thymeleaf

--- src/main/java/irfn/pi/eventos/controllers/EventosController.java
+++ src/main/java/irfn/pi/eventos/controllers/EventosController.java
@@ -16,7 +16,7 @@ public class EventosController {
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
-		return "formEvento";
+		return "eventos/formEvento";
 	}
 	
 	@PostMapping("/eventos") //metodo post
@@ -26,6 +26,6 @@ public class EventosController {
 		System.out.println(evento);
 		er.save(evento);
 		
-		return "evento-adicionar";
+		return "eventos/evento-adicionar";
 	}
 }
