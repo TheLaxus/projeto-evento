b111ae0f6ccfbd329d56ba014871f02c8cb8eab6
Lucas Barbosa <lucashp03@gmail.com>
2021-08-22 17:37:21 -0300

    Adicionando pagina de detalhes de eventos

--- src/main/java/irfn/pi/eventos/controllers/EventosController.java
+++ src/main/java/irfn/pi/eventos/controllers/EventosController.java
@@ -1,11 +1,13 @@
 package irfn.pi.eventos.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,7 +29,6 @@ public class EventosController {
 		return mv;
 	}
 
-
 	@PostMapping // metodo post
 	public String adicionar(@ModelAttribute Evento evento) {
 
@@ -37,14 +38,33 @@ public class EventosController {
 
 		return "eventos/evento-adicionar";
 	}
-	
+
 	@GetMapping
 	public ModelAndView listar() {
-		
+
 		List<Evento> eventos = er.findAll();
 		ModelAndView mv = new ModelAndView("eventos/lista");
 		mv.addObject("eventos", eventos);
 		return mv;
-		
+	}
+
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+
+		Optional<Evento> opt = er.findById(id);
+		ModelAndView mv = new ModelAndView();
+
+		// se o id for vazio
+		if (opt.isEmpty()) {
+			mv.setViewName("redirect:/eventos");
+			return mv;
+		}
+
+		mv.setViewName("eventos/detalhes");
+		Evento evento = opt.get();
+
+		mv.addObject("detalhe", evento);
+
+		return mv;
 	}
 }
