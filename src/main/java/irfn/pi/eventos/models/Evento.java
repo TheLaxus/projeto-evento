4997ccd31cde85197ea2c8a5c3c6db1096fee8b5
Lucas Barbosa <lucashp03@gmail.com>
2021-08-22 00:28:21 -0300

    Tentando implementar Lambok

--- src/main/java/irfn/pi/eventos/models/Evento.java
+++ src/main/java/irfn/pi/eventos/models/Evento.java
@@ -5,8 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity //vai criar tabela
+
 public class Evento {
+	@Getter
+	@Setter
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //gerar id automatico
@@ -16,45 +22,6 @@ public class Evento {
 	private String data;
 	private String horario;
 	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getLocal() {
-		return local;
-	}
-
-	public void setLocal(String local) {
-		this.local = local;
-	}
-
-	public String getData() {
-		return data;
-	}
-
-	public void setData(String data) {
-		this.data = data;
-	}
-
-	public String getHorario() {
-		return horario;
-	}
-
-	public void setHorario(String horario) {
-		this.horario = horario;
-	}
 
 	@Override
 	public String toString() {
